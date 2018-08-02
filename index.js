6196083941616fc356571f6663f2597168ec27b7
Christa Gabert <gabert.cm@gmail.com>
2018-08-02 17:16:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!` {
-  let name === 'you'
-}
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
